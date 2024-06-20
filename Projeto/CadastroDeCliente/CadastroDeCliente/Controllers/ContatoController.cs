06fc2d8ad615357a5f25db76fa4b76c7e96fd502
joaomorangoni <joaopedromorangoni@gmail.com>
2024-06-20 10:51:50 -0300

    fim

---
+++ Projeto/CadastroDeCliente/CadastroDeCliente/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroDeCliente.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto/CadastroDeCliente/CadastroDeCliente/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CadastroDeCliente.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
